1d7012c5246241420a1e86425b3dcfe948c63050
haru-kiku <haru11250321@gmail.com>
2023-09-24 13:48:20 +0900

    背景変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ==" crossorigin="">
-        <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link rel="stylesheet" type="text/css" href="/public/css/style.css">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+        integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+        crossorigin="">
+    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+        integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+        crossorigin=""></script>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
 
 
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
 </html>
--- resources/views/posts/top.blade.php
+++ resources/views/posts/top.blade.php
@@ -4,19 +4,27 @@
             {{ session('notice') }}
         </div>
     @endif
-    <div class="text-center mt-8 pb-9 bg-white bg-opacity-90">
+    <div class="text-center mt-8 pb-9" style="background-image: url('{{ asset('images/chusonjikouyou.jpg') }}'); background-size: cover;">
+    
         <h1 class="text-3xl font-bold mt-4">Hi-report</h1>
         <div class="w-2/3 mx-auto my-4 p-4">
         </div>
-        <div class="flex flex-col items-center justify-center space-y-4 md:space-y-8">
+        <div class="flex flex-col items-center space-y-4 md:space-y-8">
             <div class="my-4"></div>
             <a href="{{ route('posts.create') }}"
-                class="w-72 bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-10 rounded-lg text-lg">投稿する</a>
+                class="w-72 bg-green-500 hover:bg-green-700 text-white text-center font-bold py-3 px-10 rounded-lg text-lg">投稿する</a>
             <a href="{{ route('markers') }}"
-                class="w-72 bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-10 rounded-lg text-lg">対応状況</a>
+                class="w-72 bg-green-500 hover:bg-green-700 text-white text-center font-bold py-3 px-10 rounded-lg text-lg">対応状況</a>
             <a href="{{ route('emergency') }}"
-                class="w-72 bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-10 rounded-lg text-lg">緊急時(電話通報)</a>
+                class="w-72 bg-red-500 hover:bg-red-700 text-white text-center font-bold py-3 px-10 rounded-lg text-lg">緊急時(電話通報)</a>
         </div>
     </div>
-    </div>
+
 </x-app-layout>
+
+{{-- <style>
+    body {
+        background-image: url('{{ asset('images/chusonjikouyou.jpg') }}');
+        background-size: cover;
+    }
+</style> --}}
